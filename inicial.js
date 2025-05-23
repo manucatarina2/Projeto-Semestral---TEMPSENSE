880012ee34f30810f1f3d0b7f8eefd9bafcf5d0a
Manuela Catarina <manucatarinavs2@gmail.com>
2025-05-22 22:59:52 -0300

    Add files via upload

---
+++ inicial.js
@@ -0,0 +1,16 @@
+        tailwind.config = {
+            theme: {
+                extend: {
+                    colors: {
+                        'blue-primary': '#0077b6',
+                        'blue-light': '#90e0ef',
+                        'blue-navy': '#023e8a',
+                        'beige-sand': '#f5f0e1',
+                        'white-clean': '#ffffff',
+                    },
+                    fontFamily: {
+                        'montserrat': ['Montserrat', 'sans-serif'],
+                    }
+                }
+            }
+        }
---
+++ script.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'navy': '#0A3D62',
+                'blue': '#1E90FF',
+                'light-blue': '#87CEFA',
+                'sand': '#F5DEB3',
+                'off-white': '#F8F8FF'
+            },
+            fontFamily: {
+                'montserrat': ['Montserrat', 'sans-serif']
+            }
+        }
+    }
+};
